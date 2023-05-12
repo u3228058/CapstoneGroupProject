2eb4e927ef4c6d9a415ab2f3466ecf09c67203cc
AKisfkjf <133326841+AKisfkjf@users.noreply.github.com>
2023-05-12 14:00:39 +0100

    Update de_gui.py

--- Software Capcut/de_gui.py
+++ Software Capcut/de_gui.py
@@ -1,6 +1,5 @@
 import tkinter as tk
-from wc_model import *
-
+from wc_model import best_model
 
 class WC_GUI:
     def __init__(self):
@@ -13,7 +12,7 @@ class WC_GUI:
         self.two_frame = tk.Frame()
         self.three_frame = tk.Frame()
         self.four_frame = tk.Frame()
-        self.five_frame = tk.Frame()
+        self.fifth_frame = tk.Frame()
         self.six_frame = tk.Frame()
         self.seven_frame = tk.Frame()
         self.eight_frame = tk.Frame()
@@ -22,27 +21,23 @@ class WC_GUI:
         # Create title display
         self.title_label = tk.Label(self.one_frame, text='MMI predictor', fg="Blue", font=("Helvetica", 18))
         self.title_label.pack()
-        
 
         self.magnitude_label = tk.Label(self.three_frame, text='Magnitude:')
         self.magnitude_entry = tk.Entry(self.three_frame, bg="white", fg="black", width=12)
         self.magnitude_label.pack(side='left')
         self.magnitude_entry.pack(side='left')
 
-    
-        self.depth_label = tk.Label(self.four_frame, text='Depth:')
+        self.depth_label = tk.Label(self.four_frame, text='Depth (km):')
         self.depth_entry = tk.Entry(self.four_frame, bg="white", fg="black", width=10)
         self.depth_label.pack(side='left')
         self.depth_entry.pack(side='left')
 
-      
-         self.magnitude_predict_ta. = tk.Text(self.ninth_frame, height=10, width=25, bg='light blue')
+        self.magnitude_predict_ta = tk.Text(self.ninth_frame, height=10, width=25, bg='light blue')
 
-       
-        self.button_predict = tk.Button(self.ninth_frame, text='Predicate MMI', command=self.predict_Predicate_MMI)
+        self.button_predict = tk.Button(self.ninth_frame, text='Predict MMI', command=self.predict_MMI)
         self.button_quit = tk.Button(self.ninth_frame, text='Quit', command=self.main_window.destroy)
 
-        self.channel_predict_ta.pack(side='left')
+        self.magnitude_predict_ta.pack(side='left')
         self.button_predict.pack()
         self.button_quit.pack()
 
@@ -50,7 +45,7 @@ class WC_GUI:
         self.two_frame.pack()
         self.three_frame.pack()
         self.four_frame.pack()
-        self.five_frame.pack()
+        self.fifth_frame.pack()
         self.six_frame.pack()
         self.seven_frame.pack()
         self.eight_frame.pack()
@@ -58,36 +53,49 @@ class WC_GUI:
 
         tk.mainloop()
 
-    def predict_Predicate_MMI(self)\:7
+    def predict_MMI(self):
         result_str = ""
-        self.magnitude_predict_ta.delete(0.0, tk.END)
-      
-            customer_Reg = 3;
+        self.magnitude_predict_ta.delete('1.0', tk.END)
+
         earthquake_magnitude = self.magnitude_entry.get()
         earthquake_depth = self.depth_entry.get()
-       
-        result_str += "===Earthquake MMI=== \n"
-        earthquake_MMI = (earthquake_magnitude,earthquake_depth)
 
-        earthquake_prediction = best_model.predict([MMI]) #result
-        display = ("This earthquake has an MMI of:", str(model_accuracy))
+        result_str += "=== Earthquake MMI ===\n"
+        earthquake_MMI = (earthquake_magnitude, earthquake_depth)
 
-        if (channel_prediction == [0]):
-            result_str = (display, '\n', "0 - Customer should be in Hospitality Industry")
+        earthquake_prediction = best_model.predict([earthquake_MMI])
+        display = f"This earthquake has an MMI of: {earthquake_prediction}"
+        result_str = display
+
+        if earthquake_prediction == 0:
+            result_str += "\n0 - No damage"
+        elif earthquake_prediction == 1:
+            result_str += "\n1 - Minor damage"
+        elif earthquake_prediction == 2:
+            result_str += "\n2 - Moderate damage"
+        elif earthquake_prediction == 3:
+            result_str += "\n3 - Strong damage"
+        elif earthquake_prediction == 4:
+            result_str += "\n4 - Severe damage"
+        elif earthquake_prediction == 5:
+            result_str += "\n5 - Devastating damage"
         else:
-            result_str = (display, '\n' + "1 - Customer should be in retail Industry")
-        self.channel_predict_ta.insert('1.0', result_str)
+            result_str += "\nInvalid prediction"
 
-    #Testing
-    def test1():
-        customer_info = (2, 304, 135, 127, 87, 215, 77)
-        assert best_model.predict([customer_info]) == 0
+        self.magnitude_predict_ta.insert('1.0', result_str)
 
-    def test2():
-        customer_info = (2, 158, 393, 414, 263, 402, 102)
-        assert best_model.predict([customer_info]) == 1
 
-    test1()
-    test2()
-    print("Test 1 & 2 passed")
+def test1():
+    earthquake_info = (7.5, 10)
+    assert best_model.predict([earthquake_info]) == 4
+
+def test2():
+    earthquake_info = (6.2, 5)
+        assert best_model.predict([earthquake_info]) == 2
+
+test1()
+test2()
+print("Test 1 & 2 passed")
+
 my_wc_GUI = WC_GUI()
+
